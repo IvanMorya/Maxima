f1f62aa8484df19c3cc1b21fc0b4a9b2777b9ad4
moryi <mory.ivan@gmail.com>
2024-11-09 12:11:37 +0300

    Занятие по рекурсии.

--- src/lesson4/Recursion.java
+++ src/lesson4/Recursion.java
@@ -1,4 +1,22 @@
 package lesson4;
 
 public class Recursion {
+    public static void main(String[] args) {
+    }
+
+    public static long factorial(int number) {
+        long result = 1;
+        for (int i = number; i >= 1; i--) {
+
+            result = result * i;
+
+        }
+        return result;
+    }
+
+    public static long recursiveFactorial(int number) {
+        if (number <= 1) return 1;
+        return number * recursiveFactorial(number - 1);
+    }
+
 }
