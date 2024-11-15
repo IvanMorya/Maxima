4990fe1b10f0f96dafa9ad8d7461cfc476f49e74
moryi <mory.ivan@gmail.com>
2024-11-15 23:30:53 +0300

    Инкапсуляция, наследование, полиморфизм.

---
+++ src/Lesson6/Client.java
@@ -0,0 +1,14 @@
+package Lesson6;
+
+public class Client {
+
+    private Animal animal;
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+}
