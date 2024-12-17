7b07801d75942768db80ed801ed8f394ca3cc699
moryi <mory.ivan@gmail.com>
2024-12-17 13:32:02 +0300

    Списки и ассоциативные массивы, Java collection API.

--- src/lesson11/classobject/Person.java
+++ src/lesson11/classobject/Person.java
@@ -8,11 +8,17 @@ public class Person {
     private String name;
     private int age;
 
-    private Product product;
-
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     @Override
--- src/lesson12/Test.java
+++ src/lesson12/Test.java
@@ -2,19 +2,45 @@ package lesson12;
 
 import lesson11.classobject.Person;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<Person> arrayList = new ArrayList<>();
+//        arrayList.add("Orange");
+//        arrayList.add("Apple");
+//        arrayList.add("Mango");
 
-        arrayList.add("Apple");
-        arrayList.add("Apple");
-        arrayList.add("Banana");
-        arrayList.add("Banana");
+        Person egor2 = new Person("Egor", 18);
+        Person olga = new Person("Olga", 25);
+        Person egor3 = new Person("Egor", 18);
+        Person oleg = new Person("Oleg", 12);
 
-        System.out.println("ArrayList: " + arrayList);
+//        arrayList.add(egor2);
+//        arrayList.add(olga);
+//        arrayList.add(egor3);
+//        arrayList.add(oleg);
 
+
+
+        HashMap<String, Person> stringPersonHashMap = new HashMap<>();
+        stringPersonHashMap.put(egor2.getName(), egor2);
+        stringPersonHashMap.put(olga.getName(), olga);
+        stringPersonHashMap.put(egor3.getName(), egor3);
+        stringPersonHashMap.put(oleg.getName(), oleg);
+
+        System.out.println(stringPersonHashMap);
+
+        for (Map.Entry<String, Person> entry : stringPersonHashMap.entrySet()) {
+
+            var person = new Person(" ", 0);
+            entry.setValue(person);
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
     }
 
